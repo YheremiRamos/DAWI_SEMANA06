7702c79a0769c664c23415da113d47642b474691
YheremiRamos <yheremiramosquispe@gmail.com>
2024-05-04 13:03:12 -0500

    BACKEND

--- src/main/java/com/centroinformacion/repository/RevistaRepository.java
+++ src/main/java/com/centroinformacion/repository/RevistaRepository.java
@@ -1,10 +1,23 @@
 package com.centroinformacion.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.centroinformacion.entity.Revista;
 
 public interface RevistaRepository extends JpaRepository<Revista, Integer>{
 	
+	//Tecnologia ORM 
+	@Query("select e from Revista e where e.nombre like ?1")
+	public List<Revista> listaPorNombreLike(String filtro);
 
+	
+	@Query("select e from Revista e where e.nombre = ?1 ")
+	public abstract List<Revista> listaPorNombreIgualRegistra(String nombre);
+	
+	@Query("select e from Revista e where e.nombre = ?1 and e.idRevista != ?2 ")
+	public abstract List<Revista> listaPorNombreIgualActualiza(String nombre, int idRevista);
+	
 }
--- src/main/java/com/centroinformacion/service/RevistaService.java
+++ src/main/java/com/centroinformacion/service/RevistaService.java
@@ -6,7 +6,18 @@ import com.centroinformacion.entity.Revista;
 
 public interface RevistaService {
 
+	//Para el crud
 	public abstract Revista insertaActualizaRevista(Revista obj);
+	public abstract List<Revista> listaRevistaPorNombreLike(String nombre);
+	public abstract void eliminaRevista(int idRevista);
 	public abstract List<Revista> listaTodos();
+	
+	//Validaciones Para Revistrar
+	public abstract List<Revista> listaRevistaPorNombreIgualRegistra(String nombre);
+	
+	//Validaciones Para Actualizar
+	public abstract List<Revista> listaRevistaPorNombreIgualActualiza(String nombre, int idRevista);
+
+
 	
 }
